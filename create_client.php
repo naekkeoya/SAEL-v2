060824ee015c5ee8b29cfdb9abbfd4a2ca78c6e8
raya9631 <kaiser123qwe>
2019-05-05 23:35:13 -0500

    clientes y eventos

--- create_client.php
+++ create_client.php
@@ -14,7 +14,7 @@
 		$fecha=$_POST['fecha'];
 		$hora=$_POST['hora'];
 		$paquete=$_POST['paquete'];
-		$horas_extra=$_POST['horas_extra'];
+		$horas_extra=$_POST['horas_extras'];
 		$duracion = 5 + $horas_extra;
 		$query = mysqli_query($conexion, "INSERT INTO cliente (id_cliente, nombre_cliente, paterno_cliente, materno_cliente, correo_cliente, telefono_cliente) VALUES (NULL, '$nombre', '$paterno', '$materno', '$correo', '$telefono')");
 		if (!$query)
@@ -26,13 +26,27 @@
 			$query = mysqli_query($conexion, "SELECT *FROM cliente ORDER BY id_cliente DESC LIMIT 1");
 			$query = mysqli_fetch_array($query);
 			$id = $query[0];
-			$query = mysqli_query($conexion, "SELECT *FROM agentes ORDER BY id_agente DESC LIMIT 1");
+			$query = mysqli_query($conexion, "SELECT *FROM agente ORDER BY id_agente DESC LIMIT 1");
 			$query = mysqli_fetch_array($query);
 			$id_agente = $query[0];
-			$query = mysqli_query($conexion, "SELECT *FROM paquete WHERE nombre_paquete = $paquete");
-			$query = mysqli_fetch_array($query);
-			$id_paquete = $query[0];
-			$query= mysqli_query($conexion, "INSERT INTO evento (id_evento, id_cliente, id_agente, id_paquete, fecha_evento, hora_evento, duracion_evento) VALUES (null, '$id', '$id_agente', '$paquete', '$fecha', '$hora', '$duracion')");
+			if ($paquete == "Básico")
+			{
+				$id_paquete = 1;
+			}
+			if ($paquete == "D' León")
+			{
+				$id_paquete = 2;
+			}
+			if ($paquete == "D' León Plus")
+			{
+				$id_paquete = 3;
+			}
+			if ($paquete == "D' León Especial")
+			{
+				$id_paquete = 4;
+			}
+			$query= mysqli_query($conexion, "INSERT INTO evento (id_evento, id_cliente, id_agente, id_paquete, fecha_evento, hora_evento, duracion_evento) VALUES (NULL, '$id', '$id_agente', '$id_paquete', '$fecha', '$hora', '$duracion')");
+			header("refresh:0;url=booking.html"); 
 		}
 	}
 ?>
